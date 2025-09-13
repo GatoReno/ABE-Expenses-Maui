014ac4bd889f40dfc1d4e2326a924941887c6e1e
EdOphanimRawr <erp.reno@hotmail.com>
2025-09-13 16:29:35 -0400

    first commit

---
+++ AbeXP/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AbeXP;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ AbeXP/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AbeXP;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
