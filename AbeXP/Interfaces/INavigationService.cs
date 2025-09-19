75c3e925d7874c0581175c6f5c4ec73b39aadfbd
Alex Córdova <alexcordovac@outlook.com>
2025-09-19 12:13:36 -0400

    added navigation service

---
+++ AbeXP/Interfaces/INavigationService.cs
@@ -0,0 +1,9 @@
+﻿namespace AbeXP.Interfaces
+{
+    internal interface INavigationService
+    {
+        Task InitializeAsync();
+        Task NavigateToAsync(string route, IDictionary<string, object> routeParameters = null);
+        Task PopAsync();
+    }
+}
---
+++ AbeXP/Services/MauiNavigationService.cs
@@ -0,0 +1,37 @@
+﻿using AbeXP.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbeXP.Services
+{
+    internal class MauiNavigationService : INavigationService
+    {
+        private readonly ISettingsService _settingsService;
+
+        public MauiNavigationService(ISettingsService settingsService)
+        {
+            _settingsService = settingsService;
+        }
+
+        public Task InitializeAsync() =>
+            NavigateToAsync(
+                string.IsNullOrEmpty(_settingsService.AuthAccessToken)
+                    ? "//Login"
+                    : "//Main");
+
+        public Task NavigateToAsync(string route, IDictionary<string, object> routeParameters = null)
+        {
+            var shellNavigation = new ShellNavigationState(route);
+
+            return routeParameters != null
+                ? Shell.Current.GoToAsync(shellNavigation, routeParameters)
+                : Shell.Current.GoToAsync(shellNavigation);
+        }
+
+        public Task PopAsync() =>
+            Shell.Current.GoToAsync("..");
+    }
+}
