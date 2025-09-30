1a66b749d14a83606be9d230697c73c200eec9f3
Alex Córdova <alexcordovac@outlook.com>
2025-09-30 11:06:48 -0400

    Added Result pattern

---
+++ AbeXP/Common/Result/Result.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AbeXP.Common.Result
+{
+    public class Result
+    {
+        public bool IsSuccessful => Errors.Count == 0;
+        public bool IsFailed => !IsSuccessful;
+
+        public List<Error> Errors { get; protected set; } = [];
+        public List<Warning> Warnings { get; protected set; } = [];
+
+        protected Result(List<Error> errors, List<Warning> warnings)
+        {
+            Errors = errors;
+            Warnings = warnings;
+        }
+
+        public static Result Ok()
+        {
+            return new Result([], []);
+        }
+
+        public static Result Fail(params Error[] errors)
+        {
+            var validErrors = errors.Where(e => e is not null).ToList();
+            if (validErrors.Count == 0)
+            {
+                validErrors.Add(new Error("Fail result is created without any error."));
+            }
+
+            return new Result(validErrors, []);
+        }
+
+        public static implicit operator Result(Error error)
+        {
+            return Fail(error);
+        }
+
+        public static implicit operator Result(Error[] errors)
+        {
+            return Fail(errors);
+        }
+
+        public void Add(ResultBaseModel model)
+        {
+            if (model is Error)
+            {
+                Errors.Add((model as Error)!);
+
+                return;
+            }
+
+            if (model is Warning)
+            {
+                Warnings.Add((model as Warning)!);
+
+                return;
+            }
+        }
+
+        public void AddRange(IReadOnlyList<ResultBaseModel> models)
+        {
+            foreach (var model in models)
+            {
+                Add(model);
+            }
+        }
+
+    }
+}
---
+++ AbeXP/Common/Result/ResultBaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbeXP.Common.Result
+{
+    public abstract record ResultBaseModel(string Message);
+
+    public record Error(string Message) : ResultBaseModel(Message);
+
+    public record Warning(string Message) : ResultBaseModel(Message);
+}
