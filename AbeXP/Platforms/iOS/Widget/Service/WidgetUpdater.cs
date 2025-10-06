a1a5fe009d00c49678ceebc32ba71eae043f2e5c
EdOphanimRawr <erp.reno@hotmail.com>
2025-10-06 16:37:09 -0400

    bypass ios widget and svg icon added

--- AbeXP/Platforms/iOS/Widget/Service/WidgetUpdater.cs
+++ AbeXP/Platforms/iOS/Widget/Service/WidgetUpdater.cs
@@ -11,7 +11,12 @@ namespace AbeXP.Platforms.iOS.Widget.Service
     {
         public void NotifyDataChanged()
         {
-            throw new NotImplementedException();
+            
+        }
+
+        public void Redraw()
+        {
+            
         }
     }
 }
